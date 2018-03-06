61c6113b601474145c1f257342fca45787d13e80
Lipsa Panda <lpanda@remedypartners.com>
2018-03-06 10:30:53 +0000

    Added Emacs Configuration

---
+++ .emacs.d/prelude-back-theme.el
@@ -0,0 +1,10 @@
+(deftheme prelude-back
+  "Created 2018-03-06.")
+
+(custom-theme-set-variables
+ 'prelude-back
+ '(package-selected-packages (quote (elpy zop-to-char zenburn-theme yaml-mode which-key volatile-highlights undo-tree swiper smex smartrep smartparens smart-mode-line rainbow-mode rainbow-delimiters pyenv-mode-auto ov operate-on-number multiple-cursors move-text markdown-mode magit json-mode imenu-anywhere ido-ubiquitous helm-projectile helm-descbinds helm-ag guru-mode grizzl god-mode gitignore-mode gitconfig-mode git-timemachine gist geiser flycheck flx-ido expand-region elisp-slime-nav eink-theme ein editorconfig easy-kill discover-my-major diminish diff-hl deft csv-mode crux company-anaconda clojure-mode browse-kill-ring beacon anzu ace-window 4clojure)))
+ '(python-shell-interpreter "ipython")
+ '(python-shell-interpreter-args "ipython --simple-prompt -i"))
+
+(provide-theme 'prelude-back)
