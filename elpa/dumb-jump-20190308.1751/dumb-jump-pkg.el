e529045c387a93091e665795e8c27e1b045f57dd
Lipsa Panda <lpanda@remedypartners.com>
2019-07-10 10:44:55 -0400

    added some files hope this works

---
+++ elpa/dumb-jump-20190308.1751/dumb-jump-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "dumb-jump" "20190308.1751" "jump to definition for multiple languages without configuration." '((emacs "24.3") (f "0.20.0") (s "1.11.0") (dash "2.9.0") (popup "0.5.3")) :commit "25b160683aa26c2eab9c464c86ee0f4a859362cb" :keywords '("programming") :authors '(("jack angers")) :maintainer '("jack angers"))
